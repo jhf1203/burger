418a85adfb55afc4d190b2c34650bd9e39f7451b
Jim Faulkner <jhf1203@gmail.com>
2020-09-21 16:52:11 -0400

    model file completed

--- models/burger.js
+++ models/burger.js
@@ -2,4 +2,23 @@
 
 const orm = require("../config/orm.js")
 
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    insertOne(col1, col2, val1, val2, cb) {
+        orm.insertOne("burgers", col1, col2, val1, val2, function (res) {
+            cb(res);
+        });
+    },
+    updateOne(col1, valNew, col2, cId, cb) {
+        orm.updateOne("burgers", col1, valNew, col2, cId, function (res) {
+            cb(res);
+        });
+    },
+};
+
 module.exports = burger
